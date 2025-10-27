8c142014f0517064f13a9647bb78d8dcc634ff3d
Irene Sara Mathew <mirenesara@gmail.com>
2025-10-27 15:05:18 +1300

    Specflow feature file created

---
+++ TurnUpPortal/Reqnroll/TMFeature.feature
@@ -0,0 +1,12 @@
+﻿Feature: TMFeature
+
+As a TurnUp Portal Admin user
+I would like to create, edit and delet time and material records
+So that I can manage employees time and materials successfully
+
+@regression
+Scenario: create time record with valid data
+	Given I logged into TurnUp Portal successfully
+	When I navigate to Time and Material Page
+	When I create a time record
+	Then the record should be created successfully
---
+++ TurnUpPortal/StepDefinition/TMFeatureStepDefinitions.cs
@@ -0,0 +1,33 @@
+using System;
+using Reqnroll;
+
+namespace TurnUpPortal
+{
+    [Binding]
+    public class TMFeatureStepDefinitions
+    {
+        [Given("I logged into TurnUp Portal successfully")]
+        public void GivenILoggedIntoTurnUpPortalSuccessfully()
+        {
+            throw new PendingStepException();
+        }
+
+        [When("I navigate to Time and Material Page")]
+        public void WhenINavigateToTimeAndMaterialPage()
+        {
+            throw new PendingStepException();
+        }
+
+        [When("I create a time record")]
+        public void WhenICreateATimeRecord()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then("the record should be created successfully")]
+        public void ThenTheRecordShouldBeCreatedSuccessfully()
+        {
+            throw new PendingStepException();
+        }
+    }
+}
